9e50fe432eac97f0e363870e3e36f9fa7f17cff7
Gallay Lajos <gallay.lajos@gmail.com>
2021-11-18 09:03:55 +0100

    chore: updated dependencies

--- frontend/src/app-root/routes.tsx
+++ frontend/src/app-root/routes.tsx
@@ -10,90 +10,63 @@ export const Routes = () => {
     <div style={{ width: '100%', height: '100%' }}>
       <Switch>
         {user.hasRole('admin') && (
-          <Route
-            exact
-            path="/settings/system/:settingName?"
-            render={({ match }) => {
-              return (
-                <Loader
-                  load={async () => {
-                    const { SystemSettingsPage } = await import(
-                      /* webpackChunkName: "system-settings" */ '../pages/settings/system/system-settings-page'
-                    )
-                    return (
-                      <SystemSettingsPage
-                        setting={
-                          SettingsTypeNames.includes(match.params.settingName as SettingsTypeName)
-                            ? (match.params.settingName as SettingsTypeName)
-                            : 'SYSTEM'
-                        }
-                      />
-                    )
-                  }}
-                />
-              )
+          <Route path="/settings/system/:settingName?">
+            <Loader
+              load={async () => {
+                const { SystemSettingsPage } = await import(
+                  /* webpackChunkName: "system-settings" */ '../pages/settings/system/system-settings-page'
+                )
+                return (
+                  <SystemSettingsPage
+                    setting={
+                      SettingsTypeNames.includes(match.params.settingName as SettingsTypeName)
+                        ? (match.params.settingName as SettingsTypeName)
+                        : 'SYSTEM'
+                    }
+                  />
+                )
+              }}
+            />
+          </Route>
+        )}
+        <Route path="/videos">
+          {' '}
+          <Loader
+            load={async () => {
+              const { VideoPage } = await import(/* webpackChunkName: "video" */ '../pages/video/video-page')
+              return <VideoPage />
             }}
           />
-        )}
-        <Route
-          exact
-          path="/videos"
-          render={() => {
-            return (
-              <Loader
-                load={async () => {
-                  const { VideoPage } = await import(/* webpackChunkName: "video" */ '../pages/video/video-page')
-                  return <VideoPage />
-                }}
-              />
-            )
-          }}
-        />
-        <Route
-          exact
-          path="/music"
-          render={() => {
-            return (
-              <Loader
-                load={async () => {
-                  const { MusicPage } = await import(/* webpackChunkName: "music" */ '../pages/music/music-page')
-                  return <MusicPage />
-                }}
-              />
-            )
-          }}
-        />
-        <Route
-          exact
-          path="/user/profile"
-          render={() => {
-            return (
-              <Loader
-                load={async () => {
-                  const { ProfilePage } = await import(
-                    /* webpackChunkName: "user-profile" */ '../pages/user/profile-page'
-                  )
-                  return <ProfilePage />
-                }}
-              />
-            )
-          }}
-        />
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return (
-              <Loader
-                load={async () => {
-                  const { DashboardPage } = await import(/* webpackChunkName: "dashboard" */ '../pages/dashboard')
-                  return <DashboardPage />
-                }}
-              />
-            )
-          }}
-        />
-        <Route render={() => <NotFoundPage />} />
+        </Route>
+
+        <Route path="/music">
+          <Loader
+            load={async () => {
+              const { MusicPage } = await import(/* webpackChunkName: "music" */ '../pages/music/music-page')
+              return <MusicPage />
+            }}
+          />
+        </Route>
+        <Route path="/user/profile">
+          <Loader
+            load={async () => {
+              const { ProfilePage } = await import(/* webpackChunkName: "user-profile" */ '../pages/user/profile-page')
+              return <ProfilePage />
+            }}
+          />
+        </Route>
+        <Route path="/">
+          {' '}
+          <Loader
+            load={async () => {
+              const { DashboardPage } = await import(/* webpackChunkName: "dashboard" */ '../pages/dashboard')
+              return <DashboardPage />
+            }}
+          />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
       </Switch>
     </div>
   )
