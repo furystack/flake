c623ff2b2cb771e3dd669799a13ef40308ceb2d9
Gallay Lajos <gallay.lajos@gmail.com>
2021-11-22 16:46:55 +0100

    added frontend bundle-analyzer script

---
+++ frontend/webpack.bundle-analyzer.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { merge } = require('webpack-merge')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const prod = require('./webpack.prod.js')
+
+module.exports = merge(prod, {
+  plugins: [new BundleAnalyzerPlugin()],
+})
